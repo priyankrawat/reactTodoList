92591bb326a8731f7574310d2e4827e7cd9bacd3
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2023-12-08 00:37:56 +0530

    Update index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,7 +3,7 @@ import { collatedTasks } from '../constants';
 export const getTitle = (projects, projectId) =>
   projects.find(project => project.projectId === projectId);
 
-export const getCollatedTitle = (projects, key) =>
+export const getCollatedTitle = (projects, key) => 
   projects.find(project => project.key === key);
  
 export const collatedTasksExist = selectedProject =>  
