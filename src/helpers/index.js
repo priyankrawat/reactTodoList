cc6e6a0915b70b055be2767efd1653aae9380a31
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2023-12-08 00:38:01 +0530

    Update index.js

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,7 +5,7 @@ export const getTitle = (projects, projectId) =>
 
 export const getCollatedTitle = (projects, key) => 
   projects.find(project => project.key === key);
- 
+  
 export const collatedTasksExist = selectedProject =>  
   collatedTasks.find(task => task.key === selectedProject);
 
