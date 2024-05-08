c402e827e6f8d9b78081ec3bafafe905f50413a3
Priyank Rawat <30529816+priyankrawat@users.noreply.github.com>
2024-05-08 10:30:43 +0530

    Update index.js

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -8,7 +8,7 @@ export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([]); 
   const [archivedTasks, setArchivedTasks] = useState([]); 
 
-  useEffect(() => { 
+  useEffect(() => {  
     let unsubscribe = firebase 
       .firestore()
       .collection('tasks')
